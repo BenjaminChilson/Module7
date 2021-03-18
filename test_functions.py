cf009ad20c25df854e4a3fa51cd5745ada2f6ad6
Benjamin Chilson <benjameschilson@gmail.com>
2021-03-18 16:51:36 -0700

    added test test_change_grade. this test fails purposely for this assignment. Usually failed test are bad. 2 successes 1 fail as expected

--- test_functions.py
+++ test_functions.py
@@ -21,3 +21,28 @@ def test_check_password(grading_system):
     incorrect_password = "wrongPassword123"
     assert grading_system.check_password(username, correct_password) == True
     assert grading_system.check_password(username, incorrect_password) == False
+
+@pytest.fixture
+def student_user(grading_system):
+    username = "akend3"
+    password = "123454321"
+    grading_system.login(username, password)
+    return grading_system.usr
+
+@pytest.fixture
+def professor_user(grading_system):
+    username = "goggins"
+    password = "augurrox"
+    grading_system.login(username, password)
+    return grading_system.usr
+
+
+def test_change_grade(grading_system, professor_user, student_user):
+    student_name = student_user.name
+    course = "comp_sci"
+    assignment = "assignment1"
+    new_grade = 50
+    
+    professor_user.change_grade(student_name, course, assignment, new_grade)
+    grading_system.reload_data()
+    assert grading_system.users[student_name]['courses'][course][assignment]['grade'] == new_grade
